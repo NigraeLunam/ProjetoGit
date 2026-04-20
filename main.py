eb48220b9897e86efa91362c5a7b8c63cf91969e
NigraeLunam <101352918+NigraeLunam@users.noreply.github.com>
2026-04-20 20:21:41 -0300

    Inclusão das demais operações
    Inclusão das demais operações básicas da calculadora
    Subtração
    Multiplicação
    Divisão

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 print("Projeto Calculadora")
 
 a = float(input("Digite o primairo número: "))
-b = float(input("Digite o segundo número: "))b = float(input("Digite o segundo número: "))
+b = float(input("Digite o segundo número: "))
 
-print("Soma: ", a + b)
+print("Escolha a operação desejada:")
+print("1 - Soma")
+print("2 - Subtração")
+print("3 - Multiplicação")
+print("4 - Divisão")
+
+operacao = input("Número da operação: ")
+
+if operacao == "1":
+    print("Resultado da soma: ", a + b)
+elif operacao == "2":
+    print("Resultado da subtração: ", a - b)
+elif operacao == "3":
+    print("Resultado da multiplicação: ", a * b)
+elif operacao == "4":
+    print("Resultado da divisão: ", a / b)
